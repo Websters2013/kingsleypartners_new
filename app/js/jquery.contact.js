6a26916698d82fb4c4c522a0835aa79c9f3eb24f
einzweindrey <einzweindrey@gmail.com>
2017-05-05 09:46:57 +0300

    finished contact page

--- app/js/jquery.contact.js
+++ app/js/jquery.contact.js
@@ -5,15 +5,7 @@
         $('.contact__map-canvas').each(function () {
 
             new Map( $(this) );
-
         });
-
-        $('.contact__map-info').each(function () {
-
-            // new MapInfo( $(this) );
-
-        });
-
     } );
 
     var Map = function (obj) {
@@ -23,18 +15,36 @@
 
         //private properties
         var _self = this,
-            _marker = [],
-            _infowindow,
+            _mapInfoWrap = $( '.contact__map-info' ),
+            _addressBtns = _mapInfoWrap.find( '.contact__map-switcher-item' ),
+            _addressItem = _mapInfoWrap.find( '.contact__map-address' ),
+            _telItem = _mapInfoWrap.find( '.contact__map-tel' ),
+            _emailItem = _mapInfoWrap.find( '.contact__map-email' ),
+            _markers = [],
+            _map,
             _data = JSON.parse(this.mapWrap.attr('data-map')).marks,
             _zoom = JSON.parse(this.mapWrap.attr('data-map')).zoom;
 
         //private methods
         var _onEvents = function () {
 
-                google.maps.event.addDomListener(window, 'load', _initialize);
+                google.maps.event.addDomListener( window, 'load', _initMap );
 
+                _addressBtns.on({
+                    click: function () {
+
+                        if ( $( this ).hasClass( 'active' ) ) {
+
+                            return false
+
+                        } else {
+
+                            _choiceAddress( $( this ) )
+                        }
+                    }
+                });
             },
-            _initialize = function () {
+            _initMap = function () {
 
                 var mapOptions = {
                     zoom: _zoom,
@@ -43,118 +53,83 @@
                     styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#f0f3f6"},{"visibility":"on"}]}]
                 };
 
-                var map = new google.maps.Map(_self.mapWrap[0], mapOptions);
+                _map = new google.maps.Map( _self.mapWrap[0], mapOptions );
 
-                _setMarkers(map);
+                _setMarkers();
+            },
+            _choiceAddress = function ( elem ) {
+
+                var curBtn = elem,
+                    country = curBtn.data( 'country' );
+
+                _addressBtns.removeClass( 'active' );
+                curBtn.addClass( 'active' );
+
+                _setCenterMap( country )
 
             },
-            _setMarkers = function (map) {
+            _getActiveLocation = function () {
 
-                var image = {
-                    url: './img/map_marker.svg',
-                    size: new google.maps.Size(64, 96),
-                    origin: new google.maps.Point(0, 0),
-                    anchor: new google.maps.Point(0, 40)
-                };
+                _addressBtns.each( function () {
 
-                $.each(_data, function (i) {
+                    var curElem = $( this );
+
+                    if ( curElem.hasClass( 'active' ) ) {
+
+                        _setCenterMap( curElem.data( 'country' ) )
+                    }
+                } )
+            },
+            _setCenterMap = function ( country ) {
+
+                for ( var i = 0; i < _markers.length; i++ ) {
+
+                    if ( _markers[ i ][ 'country' ] === country ) {
+
+                        _map.panTo( _markers[ i ].getPosition() );
+
+                        _setAddressInfo( _markers[ i ] )
+                    }
+                }
+            },
+            _setAddressInfo = function ( info ) {
+
+                var address = info[ 'address' ],
+                    tel = info[ 'tel' ],
+                    email = info[ 'email' ];
+
+                _addressItem.html( address );
+                _telItem.html( tel );
+                _emailItem.attr( 'href', 'tel:'+tel+'' );
+                _emailItem.html( email );
+                _emailItem.attr( 'href', 'mailto:'+email+'' );
+
+            },
+            _setMarkers = function () {
+
+                $.each( _data, function ( i ) {
 
                     var curItem = this,
-                        curLatLng = new google.maps.LatLng(curItem.poi_latitude, curItem.poi_longitude);
+                        curLatLng = new google.maps.LatLng( curItem.poi_latitude, curItem.poi_longitude);
 
-                    _marker[ i ] = new google.maps.Marker({
+                    _markers[ i ] = new google.maps.Marker({
                         position: curLatLng,
-                        map: map,
-                        icon: image
+                        map: _map,
+                        icon: {
+                            url: './img/map_marker.png',
+                            size: new google.maps.Size(64, 96)
+                        },
+                        country: curItem.country,
+                        address: curItem.address,
+                        email: curItem.email,
+                        tel: curItem.tel
                     });
-
-                    // _infowindow = new google.maps.InfoWindow({
-                    //
-                    //     content:  '<div class="contact__map-image"><img src="../assets/app/dist/pic/Capture2.jpg" alt="building"></div>'
-                    //
-                    // });
-
-                    // google.maps.event.addListener( _marker[0], 'mouseover', function(){
-                    //
-                    //     if (!$('.contact__map-tooltip').length) {
-                    //
-                    //         _infowindow.open( map, _marker[0] );
-                    //
-                    //         $('.contact__map-image').parent().parent().parent().parent().addClass( 'contact__map-tooltip' );
-                    //
-                    //         setTimeout(function () {
-                    //
-                    //             $('.contact__map-tooltip').addClass('active');
-                    //
-                    //         }, 300)
-                    //
-                    //     }
-                    //
-                    // });
-                    //
-                    // google.maps.event.addListener(_infowindow, 'closeclick',function(){
-                    //
-                    //     map.setCenter(new google.maps.LatLng(curItem.poi_latitude, curItem.poi_longitude));
-                    //
-                    // });
-
                 });
-
             },
 
             _init = function () {
                 _onEvents();
-            };
-        _init();
-    };
-
-    var MapInfo = function (obj) {
-
-        //private properties
-        var _obj = obj,
-            _flag = false,
-            _openBtn = _obj.find('.contact__map-open'),
-            _window = $( window );
-
-        //private methods
-        var _onEvents = function () {
-
-                _openBtn.on({
-                    click: function(){
-
-                        if (_window.width() < 768) {
-                            _obj.toggleClass( 'open' );
-                            _openBtn.toggleClass( 'active' );
-                            _flag = true
-                        }
-
-                    }
-                });
-                _window.on({
-                    resize: function(){
-
-                        _setCondition();
-
-                    }
-                });
-            },
-            _setCondition = function () {
-
-                if ( _window.width() >= 768 ) {
-
-                    _obj.addClass( 'open' );
-                    _openBtn.addClass( 'active' );
-                    _flag = false;
-
-                }else if( _window.width() < 768 &&  !_flag ){
-                    _obj.removeClass( 'open' );
-                    _openBtn.removeClass( 'active' );
-                }
-
-            },
-            _init = function () {
-                _setCondition();
-                _onEvents();
+                _getActiveLocation();
             };
         _init();
     };
